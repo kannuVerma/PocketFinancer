d26c866c44d308ea7c2454520b751da7cda4b962
Meghana Bhat <mmeghanabhat@gmail.com>
2020-11-22 19:30:37 -0500

    aggregate expenses by month

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -11,7 +11,7 @@ router.get("/:id", async (req, res) => {
 
 
       const expenseList = await expenseData.getUserAllExpenses(req.params.id);
-      const budgetList = await budgetData.getUserAllBudgets(req.params.id);
+       const budgetList = await budgetData.getUserAllBudgets(req.params.id);
     //   const unixTimeZero = Date.parse('2020-11-31');
         const today = new Date();
       const expensesMonth = expenseList.filter(expense =>{
@@ -27,8 +27,27 @@ router.get("/:id", async (req, res) => {
      const expensesByCateogory =  _.groupBy(expensesMonth, "category");
      console.log(expensesByCateogory)
 
-      if(expenseList){
-        res.json(expenseList);
+     let categoryExpenseList = 
+     Object.keys(expensesByCateogory).map(element => {
+         console.log(expensesByCateogory);
+         const amount = expensesByCateogory[element].reduce(
+            function (acc, obj) { return acc + obj.amount; }, 0
+         )
+
+         const budget = budgetList.find( budget => budget.category === element)
+         let budgetAmount = 0;
+         if( budget && budget.amount ){
+             budgetAmount = budget.amount;
+         }
+         return {
+             amount: amount,
+             category: element,
+             budgetAmount: budgetAmount
+         };
+         //console.log(amount);
+     });
+      if(categoryExpenseList){
+        res.json(categoryExpenseList);
       }else{
           res.send("No expense data exists")
       }
