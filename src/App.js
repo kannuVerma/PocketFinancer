786fb7085b36d05ac7a994eda4ab9646ccfe5594
luyifanwork <65984397+luyifanwork@users.noreply.github.com>
2020-11-28 12:27:58 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Landing from './components/Landing';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import { Balance } from './components/balance';
-// import { Budget } from './components/budget';
-// import { BudgetTracker } from './components/BudgetTracker';
-// import { History } from './components/history';
-// import { ChartComponent } from './components/ChartComponent';
-// import { AddNewTransaction } from './components/addnewtransaction';
-// import { Incompence } from './components/incompence';
+import { Navbar } from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Balance } from './components/balance';
+import { Budget } from './components/budget';
+import { BudgetTracker } from './components/BudgetTracker';
+import { History } from './components/history';
+import { ChartComponent } from './components/ChartComponent';
+import { AddNewTransaction } from './components/addnewtransaction';
+import { Incompence } from './components/incompence';
 
 
 
@@ -22,13 +23,14 @@ const App = () => {
 		<Router>
     
 
-	<div className='App'>
+	{/* <div className='App'>
 				<header className='App-header'>
 					<h1 className='App-title'>Welcome to PocketFinancer</h1>					
 				</header>
 				<br />
 				<br />
-			<div className='App'>
+			<div className='App'> */}
+			<Navbar />
 			<div className="auth-wrapper">
         	<div className="auth-inner">
         	<Switch>
@@ -38,49 +40,22 @@ const App = () => {
 					<Route exact path='/login' component={Login} />
 					<Route exact path='/landing/:id' component={Landing} />
 
-					{/* <Route exact path="/"><Home /></Route>
+					{/* <Route exact path="/"><Home /></Route> */}
 					<Route exact path="/ChartComponent" component={ChartComponent} />
 					<Route exact path="/History" component={History} />
 					<Route exact path="/addnewtransaction" component={AddNewTransaction} />
 					<Route exact path="/incompence" component={Incompence} />
 					<Route exact path="/Budget" component={Budget} />
 					<Route exact path="/BudgetTracker" component={BudgetTracker} />
-					<Route exact path="/Balance" component={Balance} /> */}
+					<Route exact path="/Balance" component={Balance} />
             </Switch>
 			</div>
 			</div>
-			</div>
-			</div>    
+			{/* </div>
+			</div>     */}
 		</Router>
 	);
 };
-function LoginPage() {
-	return (
-	  <div className="App">
-		<h3>Build Sign Up & Login UI Template in React</h3>
-	  </div>
-	);
-}
-// function Home() {
-// 	return (
-// 	  <div>
-// 		<h1>Balance</h1>
-// 		<Balance />
-// 		<h1>Budget</h1>
-// 		<Budget />
-// 		<h1>BudgetTracker</h1>
-// 		<BudgetTracker />
-// 		<h1>ChartComponent</h1>
-// 		<ChartComponent />
-// 		<h1>History</h1>
-// 		<History />
-// 		<h1>AddNewTransaction</h1>
-// 		<AddNewTransaction />
-// 		<h1>incompence</h1>
-// 		<Incompence />
-// 	  </div>
-// 	)
-//   }
 
 export default App;
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+
+export function Navbar() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Nav</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-item nav-link" href="/budget">Budget</a>
+            <a class="nav-item nav-link" href="/balance">Balance</a>
+            <a class="nav-item nav-link" href="/BudgetTracker">BudgetTracker</a>
+            <a class="nav-item nav-link" href="/ChartComponent">Chart</a>
+            <a class="nav-item nav-link" href="/history">History</a>
+            <a class="nav-item nav-link" href="/addnewtransaction">Add Transactions</a>
+            <a class="nav-item nav-link" href="/incompence">Incompence</a>
+            </div>
+        </div>
+                <a className="nav-link" href='/Login'>Login</a>
+        </nav>
+    );
+  }
